297c4c0337b130ccd92d372899e1fa77f92dcefd
Thariq <alg70597@gmail.com>
2022-10-12 19:08:46 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+    <!-- Isi Halaman-->
+    <div class="container-fluid p-0">
+        <!-- Bagian About / Tengtang saya-->
+        <section class="resume-section" id="about">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    Muhammad Thariq Ziyad
+                    <span class="text-danger">Al-Ghazali</span>
+                </h1>
+                <div class="subheading mb-5">
+                    Jl. Stasiun No 98 Parigi, Pangandaran  · <a class="text-danger"> NPM 2019110119 </a> ·
+                    <a class="text-danger" href="alg70597@gmail.com">alg70597@gmail.com</a>
+                </div>
+                <p class="lead mb-5">Keahlian saya saat ini sering bermain video game dan sibuk dengan <a href="/portofolio">komputer</a>  </p>
